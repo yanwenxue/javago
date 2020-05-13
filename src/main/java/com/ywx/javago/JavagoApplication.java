777846f118b63cd61890e18860858c509031017a
yanwenxue@bunnymaicai.com <ywxcxt20190524>
2020-05-13 15:15:28 +0800

    feat: init

---
+++ src/main/java/com/ywx/javago/JavagoApplication.java
@@ -0,0 +1,13 @@
+package com.ywx.javago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavagoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ywx/javago/JavagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ywx.javago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
